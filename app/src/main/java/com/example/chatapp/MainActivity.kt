360ae766a1e13129d736fbef719756bcb115adc0
mena <mena.rizkalla1@gmail.com>
2025-08-04 18:03:21 +0300

    Define navigation screens
    This commit introduces a sealed class `Screen` to define the routes for various screens within the application.
    
    The defined screens are:
    - `LoginScreen`
    - `SignUpScreen`
    - `ForgotPasswordScreen`
    - `UsersScreen`
    - `ChatScreen` (with parameters for `receiverId` and `receiverName`)
    - `GlobalChatScreen`
    
    Additionally, this commit updates `MainActivity` to use `NavGraph` and integrates the new `presentationModule` for DI in `ChatApplication`.

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.chatapp.presentation.navigation.NavGraph
 import com.example.chatapp.ui.theme.ChatAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChatAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+
+                NavGraph()
             }
         }
     }
