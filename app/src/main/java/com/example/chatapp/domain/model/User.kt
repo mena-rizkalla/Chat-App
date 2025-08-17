3276ce0879047545ebf26a1c56c536557ece908e
mark <mark.rizkalla1@gmail.com>
2025-08-17 03:08:28 +0300

    Add `lastSeenTimestamp` to User model
    This commit introduces a new field, `lastSeenTimestamp`, to the `User` data class. This field will store the timestamp of when a user was last active, represented as a `Long`.

--- app/src/main/java/com/example/chatapp/domain/model/User.kt
+++ app/src/main/java/com/example/chatapp/domain/model/User.kt
@@ -3,5 +3,6 @@ package com.example.chatapp.domain.model
 data class User(
     val uid: String = "",
     val email: String = "",
-    val displayName: String = ""
+    val displayName: String = "",
+    val lastSeenTimestamp: Long = 0L,
 )
